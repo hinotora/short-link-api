7133a72fdb828b11e88c2db085e97ef2a7b79252
hinotora <igorluf93@yandex.ru>
2021-05-05 13:55:00 +0300

    prepare for tests

--- app/middleware.php
+++ app/middleware.php
@@ -6,7 +6,13 @@ use Slim\App;
 return function (App $app) {
     $env = $app->getContainer()->get(Settings::class)->key('env');
 
-    $errorMode = ($env == 'development');
+    if ($env == 'development') {
+        $errorMode = true;
+    } else if ($env == 'testing') {
+        $errorMode = true;
+    } else {
+        $errorMode = false;
+    }
 
     $app->addErrorMiddleware($errorMode,true,true);
 };
